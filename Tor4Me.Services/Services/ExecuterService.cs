6c4f11981fad812fb9d1e044b80bf6d8c87da7ee
IgorTor4me <inm2106@gmail.com>
2016-01-12 23:13:23 +0200

    Initial Commit

---
+++ Tor4Me.Services/Services/ExecuterService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tor4Me.Data;
+using System.Data.Entity;
+
+
+
+namespace Tor4Me.services.Services
+{
+    public class ExecuterService : IExecuterService
+    {
+        private DB_9EED4C_tor4meserverEntities db = new DB_9EED4C_tor4meserverEntities();
+        public List<executers> GetExecuters()
+        {
+            return db.executers.ToList();
+        }
+
+        public executers GetExecuter(int id)
+        {
+            return db.executers.Where(x => x.exeid == id).FirstOrDefault();
+        }
+
+
+        public bool SaveExecuter(executers executer)
+        {
+            try
+            {
+                db.executers.Add(executer);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+
+        public bool UpdateExecuter(long exeid, executers executer)
+        {
+            try
+            {
+                db.Entry(executer).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public bool DeleteExecuter(int id)
+        {
+            try
+            {
+                var executer = db.executers.Where(x => x.exeid == id).FirstOrDefault();
+                if (executer == null) return false;
+                db.Entry(executer).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+
+
+
+
+        /*      public List<executers> GetExecuters(int busid)
+              {
+             //     return db.executers.ToList().Where(x => x.exebusid == busid).FirstOrDefault();;
+               //   var executer= db.executers.Where(x => x.exebusid == busid).FirstOrDefault(); 
+                /*  return db.executers.ToList()
+                .Where(x => x.exebusid == busid)
+                .Select(xx => new
+                                 {
+                                   id = xx.exeid,
+                                   busid = xx.exebusid
+                                 }));*/
+        //    return db.executers.ToList().Where(x => x.exebusid == busid).SelectMany;
+        //     } 
+
+        /*   var employeeOptions = employees
+           .SelectMany(e => empOptions
+                              .Where(eo => eo.id == e.id)
+                              .Select(eo => new
+                              {
+                                id = eo.id,
+                                optionsCount = eo.optionsCount
+                              }));
+
+         foreach (var item in employeeOptions)
+           Console.WriteLine(item);
+       }*/
+    }
+}
---
+++ Tor4Me.WebApi/Controllers/executersController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tor4Me.Data;
+using Tor4Me.services;
+using Tor4Me.services.Services;
+
+
+namespace Tor4Me.WebApi.Controllers
+{
+    [RoutePrefix("api/executers")]
+    public class executersController : ApiController
+    {
+        private IExecuterService executerService = new ExecuterService();
+
+        [Route("")]
+        public IEnumerable<executers> Get()
+        {
+            return executerService.GetExecuters();
+        }
+
+        public IHttpActionResult GetById(int id)
+        {
+            var executers = executerService.GetExecuter(id);
+            if (executers == null)
+                return NotFound();
+            return Ok(executers);
+        }
+
+        public IHttpActionResult Post(executers executer)
+        {
+            bool isSave = executerService.SaveExecuter(executer);
+            if (isSave == true)
+                return Ok();
+            return BadRequest();
+        }
+
+        public IHttpActionResult Put(executers executer)
+        {
+            bool isUpdated = executerService.UpdateExecuter(executer.exeid, executer);
+            if (isUpdated == true)
+                return Ok();
+            return BadRequest();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            bool isDeleted = executerService.DeleteExecuter(id);
+            if (isDeleted == true)
+                return Ok();
+            return BadRequest();
+        }
+
+
+
+
+    }
+
+
+}
